510ad3d866f767094a7eccdc15efa58e61565684
Ignacio <shoik.xtazy@gmail.com>
2019-07-04 15:39:34 -0400

    more data to queries postgres

--- sql/db.sql
+++ sql/db.sql
@@ -12,3 +12,12 @@ CREATE TABLE IF NOT EXISTS tastks(
     done BOOLEAN,
     projectId INTEGER REFERENCES projects(id)
 );
+
+INSERT INTO projects (name, priority, description,deliverydate)
+VALUES('Make a Web App',1,'Using javascript','2019-05-12');
+
+INSERT INTO projects (name, priority, description,deliverydate)
+VALUES('Make an App',1,'Using Dart','2019-05-13');
+
+INSERT INTO projects (name, priority, description,deliverydate)
+VALUES('Make a Desktop App',2,'Using Dart','2019-05-14');
